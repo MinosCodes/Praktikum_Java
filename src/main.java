818c81f95abc82769299bbc3aa7eb0e44cdb8f00
MinosCodes <merdassimohamedamine08@gmail.com>
2025-10-19 20:08:57 +0200

    added testcase for outgoingintrest

--- src/main.java
+++ src/main.java
@@ -3,16 +3,17 @@ import bank.Transfer;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("=== Test 1: Invalid incoming interest ===");
+        System.out.println("=== Test 1: Fehler beim incoming interest ===");
         Payment p1 = new Payment("2025-10-16", 200.0, "Salary", 1.2, 0.01);
+        System.out.println("=== Test 9: Fehler beim outgoing interest ===");
+        Payment p9 = new Payment("2025-10-16", 200.0, "Salary", 0.2, 1.5);
+        System.out.println("\n=== Test 2: Transfer mit alle attribute ===");
+        Transfer p2 = new Transfer("2025-10-16", 200, "Rent", "Amine", "John");
 
-        System.out.println("\n=== Test 2: Transfer with all attributes ===");
-        Transfer p2 = new Transfer("2025-10-16", 200, "Rent", "Amine", "Ahmed");
-
-        System.out.println("\n=== Test 3: Payment constructor with 3 attributes ===");
+        System.out.println("\n=== Test 3: Payment constructor mit 3 attribute ===");
         Payment p3  = new Payment("2025-10-16", 200.0, "Salary");
 
-        System.out.println("\n=== Test 4: Transfer constructor with 3 attributes ===");
+        System.out.println("\n=== Test 4: Transfer constructer mit 3 attributes ===");
         Transfer p4 = new Transfer("2025-10-16", 200, "Rent");
 
         System.out.println("\n=== Test 5: Payment copy constructor ===");
@@ -21,12 +22,15 @@ public class main {
         System.out.println("\n=== Test 6: Transfer copy constructor ===");
         Transfer p6 = new Transfer(p2);
 
-        System.out.println("\n=== Test 7: Testing Negative Amount  ===");
+        System.out.println("\n=== Test 7: Fehler Negative Amount  ===");
         Transfer p7 = new Transfer("2025-10-16", -500, "Rent", "Amine", "Ahmed");
+
 
         System.out.println("\n=== Output of all objects ===");
         System.out.println("\n=== Object 1 ===");
         p1.printObject();
+        System.out.println("\n=== Object 9 ===");
+        p9.printObject();
         System.out.println("\n=== Object 2 ===");
         p2.printObject();
         System.out.println("\n=== Object 3 ===");
